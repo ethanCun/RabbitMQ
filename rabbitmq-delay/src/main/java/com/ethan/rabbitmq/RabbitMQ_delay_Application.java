072f8b1df09cff9dea549c34d197816e868ee2c0
macOfEthan <zz511627807@163.com>
2020-11-18 15:08:18 +0800

    1. RabbitMQ的五种模型： simple、 work queues、    fanout、 routing、 topic;
    2. Springboot继承RabbitMQ的五种消息模型： simple、work queues、 fanout、routing\ topic;
    
    3. linux下erlang、rabbitmq版本更新操作；

---
+++ rabbitmq-delay/src/main/java/com/ethan/rabbitmq/RabbitMQ_delay_Application.java
@@ -0,0 +1,13 @@
+package com.ethan.rabbitmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitMQ_delay_Application {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(RabbitMQ_delay_Application.class, args);
+    }
+}
